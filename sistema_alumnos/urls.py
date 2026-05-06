454d69a7c79e7ed8bdc43615f5d668513603cf9b
Jose Purizaga <jose.purizaga@tecsup.edu.pe>
2026-05-06 13:36:48 -0500

    Codigo completo de rutas y vistas

--- sistema_alumnos/urls.py
+++ sistema_alumnos/urls.py
@@ -1,19 +1,3 @@
-"""
-URL configuration for sistema_alumnos project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/6.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
@@ -24,6 +8,6 @@ urlpatterns = [
     path('', include('alumnos.urls')),
 ]
 
-# Servir archivos media en desarrollo
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# Esto sirve tanto para estáticos como para media en desarrollo y ayuda en la ruta de producción
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
